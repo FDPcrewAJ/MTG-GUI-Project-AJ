ec03ee46a0ce7135618adba331ff4796412cc83b
Jungels_Aiden <junga526@hsd.k12.or.us>
2024-04-03 14:51:59 -0700

    added all needed widgets without any styling

--- mtg-gui-project-aj/app.py
+++ mtg-gui-project-aj/app.py
@@ -3,10 +3,12 @@ import sys
 from PyQt6.QtWidgets import (
     QApplication,
     QCheckBox,
+    QListWidget,
     QLabel,
     QMainWindow,
     QPushButton,
     QVBoxLayout,
+    QHBoxLayout,
     QWidget,
 )
 
@@ -20,18 +22,37 @@ class MainWindow(QMainWindow):
         self.setContentsMargins(10, 10, 10, 10)
         self.resize(480, 640)
 
-        layout = QVBoxLayout()
+        main_layout = QVBoxLayout()
+        selector_layout = QHBoxLayout()
         widgets = [
-            QCheckBox,
             QLabel,
+            QListWidget,
             QPushButton,
         ]
 
-        for w in widgets:
-            layout.addWidget(w())
 
+        # Assign each widget a variable
+        title_label = QLabel("Magic: The Gathering Commander Finder")
+        color_selector = QListWidget()
+        color_selector.addItems(["White", "Blue", "Black", "Red", "Green", "Colorless"])
+        info_text = QLabel("Select which colors you want on your commander, or select none for a completely random card!")
+        search_button = QPushButton("Find a Commander!")
+        card_text = "Your Commander is:"
+        card_return = QLabel(card_text)
+
+
+        # Add the widget variables to the window
+        main_layout.addWidget(title_label)
+        main_layout.addLayout(selector_layout)
+        selector_layout.addWidget(color_selector)
+        selector_layout.addWidget(info_text)
+        main_layout.addWidget(search_button)
+        main_layout.addWidget(card_return)
+    
+
+        
         widget = QWidget()
-        widget.setLayout(layout)
+        widget.setLayout(main_layout)
 
         # Set the central widget of the Window. Widget will expand
         # to take up all the space in the window by default.

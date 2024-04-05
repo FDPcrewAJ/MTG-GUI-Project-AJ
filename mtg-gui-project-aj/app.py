a590f094a7ee44d5f09a5ca8666295bff3b899fb
Jungels_Aiden <junga526@hsd.k12.or.us>
2024-04-05 15:25:54 -0700

    added fonts and connected stylesheet

--- mtg-gui-project-aj/app.py
+++ mtg-gui-project-aj/app.py
@@ -1,6 +1,6 @@
 import sys
 
-from PyQt6.QtGui import QFont
+from PyQt6.QtGui import QFontDatabase, QFont
 from PyQt6.QtWidgets import (
     QApplication,
     QCheckBox,
@@ -22,6 +22,10 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("MTG Commander Finder")
         self.setContentsMargins(10, 10, 10, 10)
         self.resize(480, 640)
+        
+        # Load fonts
+        self.set_fonts("Bellefair-Regular.ttf")
+        self.set_fonts("Fondamento-Regular.ttf")
 
         main_layout = QVBoxLayout()
         selector_layout = QHBoxLayout()
@@ -34,12 +38,19 @@ class MainWindow(QMainWindow):
 
         # Assign each widget a variable
         title_label = QLabel("Magic: The Gathering Commander Finder")
+        title_label.setFont(QFont("Fondamento", 20))
+        
         color_selector = QListWidget()
         color_selector.addItems(["White", "Blue", "Black", "Red", "Green", "Colorless"])
+        color_selector.setFont(QFont("Bellefair"))
         info_text = QLabel("Select which colors you want on your commander, or select none for a completely random card!")
+        info_text.setFont(QFont("Bellefair"))
         search_button = QPushButton("Find a Commander!")
+        search_button.setFont(QFont("Fondamento"))
         card_text = "Your Commander is:"
-        card_return = QLabel(card_text)
+        card_name = "_________"
+        card_return = QLabel(f"{card_text} \n {card_name}")
+        card_return.setFont(QFont("Bellefair"))
 
 
         # Add the widget variables to the window
@@ -59,8 +70,26 @@ class MainWindow(QMainWindow):
         # to take up all the space in the window by default.
         self.setCentralWidget(widget)
 
+    
+    def set_fonts(self, font_name: str) -> None:
+        font_dir = "resources/"
+        font_path = font_dir + font_name
+        success = QFontDatabase.addApplicationFont(font_path)
+        
+        # If it failed to set a font path
+        if success == -1:
+            print(f"{font_name} not loaded: \nTried path {font_path}")
+
 
 app = QApplication(sys.argv)
+
+stylesheet = None
+styles_path = "resources/styles.qss"
+# Get the code from the stylesheet
+with open(styles_path, "r") as f:
+    stylesheet = f.read()
+app.setStyleSheet(stylesheet)
+
 app.setStyle("Fusion")
 app.setFont(QFont("Calibri", 10))
 window = MainWindow()

45d0871278a40ddc5fb4a1d198c66a8d08c707f8
Jungels_Aiden <junga526@hsd.k12.or.us>
2024-05-16 15:32:38 -0700

    worked on fixing api call not choosing proper colors

--- mtg-gui-project-aj/controller.py
+++ mtg-gui-project-aj/controller.py
@@ -15,16 +15,28 @@ def get_colors(selected_colors):
     for item in selected_colors:
         if item == "White":
             c_w = "+c%3Awhite"
-        else:
-            c_w = "-m:w"
+        #else:
+            #c_w = ""
+        
         if item == "Blue":
             c_u = "+c%3Ablue"
+        #else:
+            #c_u - ""
+        
         if item == "Black":
             c_b = "+c%3Ablack"
+        #else:
+            #c_b = ""
+        
         if item == "Red":
             c_r = "+c%3Ared"
+        #else:
+            #c_r = ""
+        
         if item == "Green":
             c_g = "+c%3Agreen"
+        #else:
+            #c_g = ""
     
     search_colors = c_w + c_u + c_b + c_r + c_g
     make_call(search_colors)
@@ -32,6 +44,6 @@ def get_colors(selected_colors):
 def make_call(search_colors):
     base_url = "https://api.scryfall.com/cards/random?q=is%3Acommander"
     full_url = base_url + search_colors
-    response = requests.get(full_url)
-    print(response.json())
-    
+    response = requests.get(full_url).json()
+    print(response)
+
